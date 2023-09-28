1f792b47e3b7bb7740a19479d17712f464132940
Daniil Derbas <dannydeceiver@gmail.com>
2023-09-28 20:02:02 +0300

    Feature: Create basic google oath flow on server

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+require('./models/user')
+require('./services/passport')
+
+mongoose.connect(process.env.MONGO_URI)
+
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' })
-})
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [process.env.COOKIE_KEY]
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/auth')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
